7bbddcc759053d1548cde6d06cc2c04da877fd24
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2023-09-29 22:06:34 -0400

    Create classchallenge.py

---
+++ classchallenge.py
@@ -0,0 +1,21 @@
+
+#create class
+class Amphibian:
+
+    def __init__(self, amphibianType,amphibianColor):
+        self.type = amphibianType
+        self.color = amphibianColor
+
+jake = Amphibian("Salamander","black and yellow")
+
+print("This amphibian is a",jake.type,"and he is colored",jake.color)
+    
+
+
+
+
+        
+
+
+
+    
