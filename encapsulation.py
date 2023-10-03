7dfb7235ca38ca649feed876a8db20c85de2ed1b
tylerlynn13 <99543167+tylerlynn13@users.noreply.github.com>
2023-10-02 22:24:54 -0400

    Update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -3,12 +3,15 @@
 class Protected:
     def __init__(self):
         self.__privateVar = 12
+        self._protectedVar = 13
 
     def getPrivate(self):
         print(self.__privateVar)
+        print(self._protectedVar)
 
     def setPrivate(self, private):
         self.__privateVar = private
+        self._protectedVar = Protected
 
 
 obj = Protected()
